589cb92c691aeb2a13204c6b986723956aefd3de
tai-kun <taikun.dev@gmail.com>
2024-03-18 11:48:34 +0900

    chore(types): Improve definition types

--- src/types/definition.ts
+++ src/types/definition.ts
@@ -86,25 +86,43 @@ export type Transition<
   G extends string,
   _T = keyof Get<D, ["states"]>,
 > =
-  | _T
-  | {
-      /**
-       * The state value to transition to.
-       */
-      readonly target: _T
-      /**
-       * The guards to check before the transition.
-       * Combines the results of multiple guard functions using the logical AND operator.
-       * 
-       * **Note: An empty guards will be `true`.**
-       */
-      readonly guard?:
-        // If no guards are defined, return an error.
-          [G] extends [never]
-        ? "Error: Guards are not defined."
-        // Else, return the guard.
-        : Guard<G>
-    }
+  // If no guards are defined, return an error.
+    [_T] extends [never]
+  ? "Error: State values are not defined."
+  // If the state value is not a literal string, return an error.
+  : IsLiteralString<_T> extends false
+  ? "Error: State values must be literal strings."
+  // If the state value is reserved, return an error.
+  : Extends<_T, ReservedKeyword> extends true
+  ? `Error: State value '${Extract<_T, string>}' is reserved.`
+  // Else, return the transition.
+  : (
+      | _T
+      | {
+          /**
+           * The state value to transition to.
+           */
+          readonly target: _T
+          /**
+           * The guards to check before the transition.
+           * Combines the results of multiple guard functions using the logical AND operator.
+           * 
+           * **Note: An empty guards will be `true`.**
+           */
+          readonly guard?:
+            // If no guards are defined, return an error.
+              [G] extends [never]
+            ? "Error: Guards are not defined."
+            // If the guard is not a literal string, return an error.
+            : IsLiteralString<G> extends false
+            ? "Error: Guards must be literal strings."
+            //  If the guard is reserved, return an error.
+            : Extends<G, ReservedKeyword> extends true
+            ? `Error: Guard name '${G}' is reserved.`
+            // Else, return the guard.
+            : Guard<G>
+        }
+    )
 
 export namespace Transition {
   export type Signature = Tagged<string, "StateValue"> | {
@@ -144,20 +162,22 @@ export type On<
   D,
   P extends ReadonlyArray<keyof any>,
   G extends string,
-> = {
-  readonly [T in keyof Get<D, P>]:
-    // If the event is not a literal string, return an error.
-      IsLiteralString<T> extends false
-    ? "Error: Event type must be a literal string."
-    // If the event is reserved, return an error.
-    : T extends ReservedKeyword
-    ? `Error: Event type '${T}' is reserved.`
-    // If the event is not defined in `$schema.events`, return an error.
-    : Extends<T, _EventTypeConstraint<D>> extends false
-    ? "Error: Event type is not defined in `$schema.events`."
-    // Else, return the transition.
-    : Transition<D, G>
-}
+> =
+  // If the event is not a literal string, return an error.
+    IsLiteralString<keyof Get<D, P>> extends false
+  ? "Error: Event types must be literal strings."
+  // Else, return the transitions.
+  : {
+      readonly [T in keyof Get<D, P>]:
+        // If the event is reserved, return an error.
+          T extends ReservedKeyword
+        ? `Error: Event type '${T}' is reserved.`
+        // If the event is not defined in `$schema.events`, return an error.
+        : Extends<T, _EventTypeConstraint<D>> extends false
+        ? "Error: Event type is not defined in `$schema.events`."
+        // Else, return the transition.
+        : Transition<D, G>
+    }
 
 export namespace On {
   export type Signature = {
@@ -201,6 +221,9 @@ export type State<
     // If no effects are defined, return an error.
       [E] extends [never]
     ? "Error: Effects are not defined."
+    // If the effect is not a literal string, return an error.
+    : IsLiteralString<E> extends false
+    ? "Error: Effects must be literal strings."
     // Else, return the effect.
     : Effect<E>
 }
@@ -218,6 +241,8 @@ export namespace State {
   }
 }
 
+declare const ERROR: unique symbol
+
 /**
  * The type of context for state machine.
  * 
@@ -228,7 +253,7 @@ export type Context<D> =
     Get<D, ["$schema", "strict"]> extends true
   ? "context" extends keyof Get<D, ["$schema"]>
     ? Get<D, ["$schema", "context"]>
-    : "Error: `context` is not defined in `$schema`."
+    : { [ERROR]: "Error: `context` is not defined in `$schema`." }
   // Else, the context can be any type.
   : Get<
       // If the context schema is defined, return the context.
@@ -277,23 +302,25 @@ export type Schema<
   /**
    * The event types that are allowed in the state machine.
    */
-  readonly events?: {
-    [T in keyof _E]:
-      // If the event type is not a literal string, return an error.
-        IsLiteralString<T> extends false
-      ? "Error: Event type must be a literal string."
-      // If the event type is reserved, return an error.
-      : T extends ReservedKeyword
-      ? `Error: Event type '${T}' is reserved.`
-      // If the event is not a plain object, return an error.
-      : IsPlainObject<_E[T]> extends false
-      ? "Error: Event must be a plain object."
-      // If the event has a property `type`, return an error.
-      : "type" extends keyof _E[T]
-      ? "Error: Event cannot have a reserved property 'type'."
-      // Else, return the event.
-      : _E[T]
-  }
+  readonly events?:
+    // If the event type is not a literal string, return an error.
+      IsLiteralString<keyof _E> extends false
+    ? "Error: Event type must be a literal string."
+    // Else, return the event type.
+    : {
+        [T in keyof _E]:
+          // If the event type is reserved, return an error.
+            T extends ReservedKeyword
+          ? `Error: Event type '${T}' is reserved.`
+          // If the event is not a plain object, return an error.
+          : IsPlainObject<_E[T]> extends false
+          ? "Error: Event must be a plain object."
+          // If the event has a property `type`, return an error.
+          : "type" extends keyof _E[T]
+          ? "Error: Event cannot have a reserved property 'type'."
+          // Else, return the event.
+          : _E[T]
+      }
   /**
    * The context of the state machine.
    */
@@ -321,22 +348,27 @@ export type Shape<
     // If no states are defined, return an error.
       [keyof _S] extends [never]
     ? "Error: No states defined."
+    // If the state value is not a literal string, return an error.
+    : IsLiteralString<keyof _S> extends false
+    ? "Error: States have no literal string value."
     // Else, return the initial state value.
     : keyof _S
   /**
    * The state definitions.
    */
-  readonly states: {
-    readonly [V in keyof _S]:
-      // If the state value is not a literal string, return an error.
-        IsLiteralString<V> extends false
-      ? "Error: State value must be a literal string."
-      // If the state value is reserved, return an error.
-      : V extends ReservedKeyword
-      ? `Error: State value '${V}' is reserved.`
-      // Else, return the state definition.
-      : State<D, ["states", V], G, E>
-  }
+  readonly states:
+    /// If the state value is not a literal string, return an error.
+      IsLiteralString<keyof _S> extends false
+    ? "Error: State values must be literal strings."
+    // Else, return the state definition.
+    : {
+        readonly [V in keyof _S]:
+          // If the state value is reserved, return an error.
+            V extends ReservedKeyword
+          ? `Error: State value '${V}' is reserved.`
+          // Else, return the state definition.
+          : State<D, ["states", V], G, E>
+      }
   /**
    * The definition how a state machine will transition when it receives a specific event.
    */
@@ -382,10 +414,239 @@ export type Signature = {
 }
 
 if (cfgTest && cfgTest.url === import.meta.url) {
-  // const { expectType } = await import("tsd")
+  const { expectType } = await import("tsd")
   const { describe, test } = cfgTest
 
   describe("src/types/definition", () => {
-    test.todo("Should be tested")
+    describe("Transition", () => {
+      test("should be inferred as an error when no states are defined", () => {
+        type Expected = "Error: State values are not defined."
+        type Actual = Transition<{}, never>
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("should be inferred as an error when the state values are not literal strings", () => {
+        type Expected = "Error: State values must be literal strings."
+        type Actual = Transition<{ states: { [key: string]: {} } }, never>
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("should be inferred as an error when the state value is reserved", () => {
+        type Def = {
+          states: {
+            $init: {}
+          }
+        }
+        type Expected = "Error: State value '$init' is reserved."
+        type Actual = Transition<Def, never>
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("`guard` should be inferred as an error when no guards are defined", () => {
+        type Def = {
+          states: {
+            active: {
+              target: "active"
+              guard: "guard"
+            }
+          }
+        }
+        type Expected = "active" | {
+          readonly target: "active"
+          readonly guard?: "Error: Guards are not defined."
+        }
+        type Actual = Transition<Def, never>
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("`guard` should be inferred as an error when the guard is not a literal string", () => {
+        type Def = {
+          states: {
+            active: {
+              target: "active"
+              guard: "guard"
+            }
+          }
+        }
+        type Expected = "active" | {
+          readonly target: "active"
+          readonly guard?: "Error: Guards must be literal strings."
+        }
+        type Actual = Transition<Def, string>
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("`guard` should be inferred as an error when the guard is reserved", () => {
+        type Def = {
+          states: {
+            active: {
+              target: "active"
+              guard: "Error:"
+            }
+          }
+        }
+        type Expected = "active" | {
+          readonly target: "active"
+          readonly guard?: "Error: Guard name 'Error:' is reserved."
+        }
+        type Actual = Transition<Def, "Error:">
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("should be inferred as an Transition object", () => {
+        type Expected = "active" | {
+          readonly target: "active"
+          readonly guard?: Guard<"guard">
+        }
+        type Actual = Transition<{ states: { active: {} } }, "guard">
+
+        expectType<Expected>({} as Actual)
+      })
+    })
+
+    describe("On", () => {
+      test("should be inferred as an error when the event types are not literal strings", () => {
+        type Expected = "Error: Event types must be literal strings."
+        type Actual = On<{}, ["on"], never>
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("should be inferred as an error when the event types are reserved", () => {
+        type Def = {
+          on: {
+            $init: {}
+            "Error:": {}
+            validEvent: {}
+          }
+        }
+        type Expected = {
+          readonly $init: "Error: Event type '$init' is reserved."
+          readonly "Error:": "Error: Event type 'Error:' is reserved."
+          readonly validEvent: Transition<Def, never>
+        }
+        type Actual = On<Def, ["on"], never>
+
+        expectType<Expected>({} as Actual)
+      })
+    })
+
+    describe("State", () => {
+      test("`effect` should be inferred as an error when no effects are defined", () => {
+        type Def = {
+          states: {
+            active: {
+              effect: "effect"
+            }
+          }
+        }
+        type Expected = "Error: Effects are not defined." | undefined
+        type Actual = State<Def, ["states", "active"], never, never>["effect"]
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("`effect` should be inferred as an error when the effect is not a literal string", () => {
+        type Def = {
+          states: {
+            active: {
+              effect: "effect"
+            }
+          }
+        }
+        type Expected = "Error: Effects must be literal strings." | undefined
+        type Actual = State<Def, ["states", "active"], never, string>["effect"]
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("`effect` should be inferred as an Effect object", () => {
+        type Def = {
+          states: {
+            active: {
+              effect: "effect"
+            }
+          }
+        }
+        type Expected = "effect" | readonly "effect"[] | undefined
+        type Actual = State<Def, ["states", "active"], never, "effect">["effect"]
+
+        expectType<Expected>({} as Actual)
+      })
+    })
+
+    describe("Context", () => {
+      test("should be inferred as an error when `context` is not defined in `$schema` in strict-mode", () => {
+        type Def = {
+          $schema: {
+            strict: true
+          }
+        }
+        type Expected = { [ERROR]: "Error: `context` is not defined in `$schema`." }
+        type Actual = Context<Def>
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("should prefer the context schema over the context", () => {
+        type Def = {
+          $schema: {
+            context: { a: 1 }
+          }
+          context: { a: 2 }
+        }
+        type Expected = { a: 1 }
+        type Actual = Context<Def>
+
+        expectType<Expected>({} as Actual)
+      })
+    })
+
+    describe("Shape", () => {
+      test("`initial `should be inferred as an error when no states are defined", () => {
+        type Expected = "Error: No states defined."
+        type Actual = Shape<{ states: {} }, never, never>["initial"]
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("`initial` should be inferred as an error when the state values are not literal strings", () => {
+        type Expected = "Error: States have no literal string value."
+        type Actual = Shape<{ states: { [key: number]: {} } }, never, never>["initial"]
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("`states` should be inferred as an error when the state values are not literal string", () => {
+        type Expected = "Error: State values must be literal strings."
+        type Actual = Shape<{ states: { [key: string]: {} } }, never, never>["states"]
+
+        expectType<Expected>({} as Actual)
+      })
+
+      test("`states` should be inferred as an error when the state value is reserved", () => {
+        type Def = {
+          states: {
+            $init: {}
+            "Error:": {}
+            validValue: {}
+          }
+        }
+        type Expected = {
+          readonly $init: "Error: State value '$init' is reserved."
+          readonly "Error:": "Error: State value 'Error:' is reserved."
+          readonly validValue: State<Def, ["states", "validValue"], never, never>
+        }
+        type Actual = Shape<Def, never, never>["states"]
+
+        expectType<Expected>({} as Actual)
+      })
+    })
   })
 }
