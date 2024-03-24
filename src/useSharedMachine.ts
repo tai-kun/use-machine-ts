5021f01311698a7b9cec78dbb5b62815979963cf
tai-kun <taikun.dev@gmail.com>
2024-03-24 09:40:08 +0900

    feat: Add optional getServerState parameter to useSharedMachine

--- src/useSharedMachine.ts
+++ src/useSharedMachine.ts
@@ -2,7 +2,20 @@ import { useIsMounted, useSyncState } from "./core/logic"
 import { useSyncExternalStore } from "./core/react"
 import type { Send, SharedMachine, State } from "./types"
 
-function useSharedMachine<D>(machine: SharedMachine<D>): [
+/**
+ * Use a shared state machine.
+ *
+ * @template D - The type of state machine definition.
+ * @param machine - The shared state machine.
+ * @param getServerState - A function that returns the state on the server.
+ * @returns An array with two elements:
+ * - The first element is the current state of the state machine.
+ * - The second element is a function that sends an event to the state machine.
+ */
+function useSharedMachine<D>(
+  machine: SharedMachine<D>,
+  getServerState?: () => State<D>,
+): [
   state: State<D>,
   send: Send<D>,
 ] {
@@ -17,7 +30,7 @@ function useSharedMachine<D>(machine: SharedMachine<D>): [
   const state = useSyncExternalStore(
     subscribe,
     getState,
-    getState,
+    (getServerState as () => State.Signature) || getState,
   ) as State.Signature
 
   useSyncState(def, conf, state, dispatch, isMounted)
